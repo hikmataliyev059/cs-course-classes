d4402e31c131104af7bedff27506f87a9c32bb84
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-09 16:46:41 +0400

    initial commit

---
+++ cs-course-classes/src/main/cs/az/classes/intro/HelloWorld.cs
@@ -0,0 +1,9 @@
+﻿namespace cs_course_classes.main.cs.az.classes.intro;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
