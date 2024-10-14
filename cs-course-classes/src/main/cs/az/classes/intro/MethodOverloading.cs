97190d18e34bd19ebe5263d3b8ee170f3e79e087
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-14 16:59:01 +0400

    add method overloading for int and double addition

---
+++ cs-course-classes/src/main/cs/az/classes/intro/MethodOverloading.cs
@@ -0,0 +1,24 @@
+﻿namespace cs_course_classes.main.cs.az.classes.intro;
+
+public class MethodOverloading
+{
+    
+    static int PlusMethodInt(int x, int y)
+    {
+        return x + y;
+    }
+
+    static double PlusMethodDouble(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static void Main(string[] args)
+    {
+        int myNum1 = PlusMethodInt(8, 5);
+        double myNum2 = PlusMethodDouble(4.3, 6.26);
+        Console.WriteLine("Int: " + myNum1);
+        Console.WriteLine("Double: " + myNum2);
+    }
+    
+}
