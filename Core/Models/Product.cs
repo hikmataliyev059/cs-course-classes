94adb928dfeb2ec79ad4c150a780133ae71848b1
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-10-26 19:46:13 +0400

    add product class to represent items with name, price, and type

---
+++ Core/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using Core.Helpers.Enums;
+
+namespace Core.Models;
+
+public class Product
+{
+    private static int _noCounter;
+
+    public int No { get; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public Types Type { get; }
+
+    public Product(string name, double price)
+    {
+        No = ++_noCounter;
+        Name = name;
+        Price = price;
+    }
+    
+}
